046cad46c56416039d354325dc2f22adde62634d
Austen Haymond <ahaymon17@gmail.com>
2019-12-17 18:46:50 -0700

    added js file

---
+++ random.js
@@ -0,0 +1,23 @@
+//Password Generator//
+const length = prompt("How many characters long would you like your password to be?")
+const lower = confirm("Do you want to use lower-case letters?")
+const upper = confirm("Do you want to use upper-case letters?")
+const num = confirm("Do you want to use numbers?")
+const special = confirm("Do you want to use special characters?")
+const password = document.getElementById("#password")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
